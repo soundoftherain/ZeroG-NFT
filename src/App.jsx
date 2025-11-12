aa3a810465e91d7c7fb83579fd113ee49ab008cf
Moses Builder <noahmoses@proton.me>
2025-11-12 11:50:06 +0100

    feat: add main App component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UploadCard from "./components/UploadCard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <UploadCard />
+    </div>
+  );
+}
